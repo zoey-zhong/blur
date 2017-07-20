bf457923861804cee19c4494b05dba3c67d4aca2
zoey-zhong <zoey2099@163.com>
2017-07-20 14:06:48 +0800

    renew
    renew

--- js/index.js
+++ js/index.js
@@ -1,69 +1,54 @@
-window.onload=function(){
+$(function() {
 
-var h=$(window).height();
-$("header").css("height",h)
-$(window).resize(function(){
-	var h=$(window).height();
-	$("header").css("height",h)
-})
+	var h = $(window).height();
 
-$("aside").click(function(){
-	$(document).scrollTop(0);
-})
+	$("aside").click(function() {
+		$(document).scrollTop(0);
+	})
 
-$(document).scroll(function(){
-	var top=$(document).scrollTop();
-	
-	if(top>10){		
+	$(document).scroll(function() {
+		var top = $(document).scrollTop();
+
+		if(top > 10) {
+			$(".navbar").addClass("change");
+			$("#navbar-collapse ul li").addClass("change2");
+			$(".navbar-logo").addClass("change3");
+			$(".navbar-brand").addClass("change4");
+			$(".navbar-header span").css("background", "white")
+		}
+		if(top < 10) {
+			$(".navbar").removeClass("change");
+			$("#navbar-collapse ul li").removeClass("change2");
+			$(".navbar-logo").removeClass("change3");
+			$(".navbar-brand").removeClass("change4");
+			$(".navbar-header span").css("background", "#000000")
+		}
+		if(top > 200) {
+			$("aside").show();
+		} else {
+			$("aside").hide()
+		}
+
+	})
+
+	$(".xs-icon").click(function() {
 		$(".navbar").addClass("change");
 		$("#navbar-collapse ul li").addClass("change2");
 		$(".navbar-logo").addClass("change3");
 		$(".navbar-brand").addClass("change4");
-		$(".navbar-header span").css("background","white")
-	}
-	if(top<10){
-		$(".navbar").removeClass("change");
-		$("#navbar-collapse ul li").removeClass("change2");
-		$(".navbar-logo").removeClass("change3");
-		$(".navbar-brand").removeClass("change4");
-		$(".navbar-header span").css("background","#000000")
-	}
-	if(top>200){
-		$("aside").show();
-	}else{$("aside").hide()}
-	
-})
+		$(".navbar-header span").css("background", "white");
 
-$(".xs-icon").click(function(){
-	$(".navbar").addClass("change");
-	$("#navbar-collapse ul li").addClass("change2");
-		$(".navbar-logo").addClass("change3");
-		$(".navbar-brand").addClass("change4");
-		$(".navbar-header span").css("background","white");
+	})
+
+	//
+	$(".down").click(function() {
+		$(document).scrollTop(h);
+	})
+
+	//轮播
+	$("#mycarousel").carousel({
+		interval: 1000,
+		wrap: true,
+	})
 
 })
-
-//
-$(".down").click(function(){
-	$(document).scrollTop(h);
-})
-
-
-
-
-//轮播
-$("#mycarousel").carousel({
-	interval:1000,
-	wrap:true,
-})
-
-
-
-
-
-
-
-
-
-
-}
